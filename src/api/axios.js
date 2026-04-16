a182711dc70970ab6d4654caa62bef49e8ef964e
Surajgupta000 <suraj4475@gmail.com>
2026-04-16 20:59:32 +0530

    deployment updates

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // If the app is running on Vercel/Production, use the Render URL. Otherwise, use localhost.
   baseURL: import.meta.env.MODE === "production" 
-    ? "https://arinya-shree-backend.onrender.com/api" 
+    ? "https://arinya-backend.onrender.com/api" 
     : "http://localhost:5000/api",
 });
 
